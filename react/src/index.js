d3b405c013c97a7826f51821a110b7db35367430
Andrew Park <atpark333@gmail.com>
2021-11-21 23:01:22 +0900

    fix: Make container div for app component

--- react/src/index.js
+++ react/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App.jsx';
+
 ReactDOM.render(
-  React.createElement('div', null, 'Photo Viewer'),
+  //React.createElement('div', null, 'Photo Viewer'),
+  <App />,
   document.getElementById('app')
 );
