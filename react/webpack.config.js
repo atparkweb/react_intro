11bff1a380fd408a95c30db994336fb279bc5544
Andrew Park <atpark333@gmail.com>
2021-11-21 21:44:40 +0900

    build: Configure babel loader

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -6,5 +6,25 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults"
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
   }
 }
